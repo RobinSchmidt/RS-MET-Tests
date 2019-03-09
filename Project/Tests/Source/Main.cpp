e4f2ff9ac1f215c5087f4e25ef7748eb7e56b272
Robin Schmidt <robin@rs-met.com>
2019-03-09 13:05:07 +0100

    added loadSample function

--- Project/Tests/Source/Main.cpp
+++ Project/Tests/Source/Main.cpp
@@ -11,12 +11,39 @@
 #include "../JuceLibraryCode/JuceHeader.h"
 
 
+std::vector<double> loadSample(const std::string& name, double* sampleRate = nullptr)
+{
+  std::string sampleDir = "../../../../Data/Samples/";
+  std::string path      = sampleDir + name + ".wav";
+
+  int numChannels = 0, numFrames = 0, iSampleRate = 0;
+  double** data = rosic::readFromWaveFile(path.c_str(), numChannels, numFrames, iSampleRate);
+
+  if(sampleRate != nullptr)
+    *sampleRate = (double) iSampleRate;
+
+  std::vector<double> v;
+
+  // copy data to vector, clean up memory..
+
+
+  return v;
+}
+
+
+
 //==============================================================================
 int main (int argc, char* argv[])
 {
 
-    // ..your code goes here!
+  std::vector<double> x;
+  double fs;
+
+  loadSample("bell_2a", &fs);
 
 
-    return 0;
+  // todo: check for memleaks (maybe move the memleak checking code to rosic)
+
+    
+  return 0;
 }
